275e0b338ea389e2efe7a52d5ae813862cd8fe01
vasiandemika <demikavasian@gmail.com>
2023-05-04 01:27:24 +0200

    auth fix

---
+++ BackEnd/src/main/java/com/patagonia/web/entity/Admin.java
@@ -0,0 +1,126 @@
+package com.patagonia.web.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "Name")
+    private String name;
+
+    @Column(name = "Surname")
+    private String surname;
+
+    @Column(name = "Username")
+    private String username;
+
+    @Column(name = "Password")
+    private String password;
+
+    @Column(name = "Email")
+    private String email;
+
+    @Column(name = "Address")
+    private String address;
+
+    @Column(name = "Gender")
+    private String gender;
+
+    @Column(name = "Age")
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    public Admin() {}
+
+    public Admin(String name, String surname, String username, String password, String email, String address, String gender, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.address = address;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
