5baeff7e2c31c98dcf00385b4aaf9f4e756b8087
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2023-06-05 23:30:54 +0200

    New organization of the project

--- PatagoniaEstate/PHP/connect_to_database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$db_url='localhost';
-$db_username='root';
-$db_password='';
-$db_name='real_estate';
-$conn =new mysqli($db_url, $db_username, $db_password, $db_name);
-
-?>
--- PatagoniaEstate/view-php/a.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pass = "pass";
-$hash = password_hash($pass, PASSWORD_BCRYPT);
-echo $hash;
-echo '"'.password_verify($pass, $hash).'"';
-?>
